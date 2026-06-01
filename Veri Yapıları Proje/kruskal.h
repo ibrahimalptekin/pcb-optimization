827b32dab283bda6e4714e590878299b6a663a99
ahmetgolcuq <ahmetgolcuq@gmail.com>
2026-06-01 21:09:27 +0300

    temizlik
    gereksiz dosyaları silme

--- Veri Yapıları Proje/kruskal.h
+++
@@ -1,8 +0,0 @@
-#ifndef KRUSKAL_H
-#define KRUSKAL_H
-
-#include "graph.h"
-
-void KruskalMST(Graph* graph);
-
-#endif
--- Veri Yapıları Proje/unionfind.h
+++
@@ -1,7 +0,0 @@
-#ifndef UNIONFIND_H
-#define UNIONFIND_H
-
-int find(int parent[], int i);
-void Union(int parent[], int x, int y);
-
-#endif
