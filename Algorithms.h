a4b17273167366f2806eb48429c219221a9d29f9
ahmetgolcuq <ahmetgolcuq@gmail.com>
2026-06-01 21:03:07 +0300

    read me eklendi
    gerekli raylib dosyası hakkında bilgi

--- Algorithms.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALGORITHMS_H
-#define ALGORITHMS_H
-
-#include "Graph.h"
-
-// Kişi 2: Kruskal veya Prim algoritmasını çalıştıracak MSTOptimizer sınıfı buraya yazılacak.
-
-#endif
--- Graph.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAPH_H
-#define GRAPH_H
-
-#include "DataStructures.h"
-
-// Kişi 1: PCBGraph sınıfı, düğüm (vertex) ve kenar (edge) yapıları buraya yazılacak.
-// BFS ve DFS fonksiyon tanımları burada yer alacak.
-
-#endif
