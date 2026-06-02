9d455a66e2eff64518aa78a35d72d4c81d6a7e3e
ibrahimalptekin <sagban.783g@gmail.com>
2026-06-02 23:56:05 +0300

    .gitignore eklendi, arayüz son hali oluşturuldu.

--- main.cpp
+++ main.cpp
@@ -1,68 +1,67 @@
 #include "raylib.h"
 #include "Graph.h"
 #include "Algorithms.h"
-#include <vector>
 #include <string>
 
-struct VisNode {
-    int id;
-    int x;
-    int y;
-};
-
 int main() {
-
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "PCB Baglanti Agi Optimizasyonu - Raylib");
+    const int screenWidth = 1024;
+    const int screenHeight = 768;
+    InitWindow(screenWidth, screenHeight, "PCB Optimizasyonu - Etkilesimli Arayuz");
     SetTargetFPS(60);
 
-    int nodeCount = 5;
-    PCBGraph pcb(nodeCount);
-    
-    std::vector<VisNode> nodes = {
-        {0, 150, 150},
-        {1, 400, 100},
-        {2, 650, 200},
-        {3, 250, 450},
-        {4, 550, 400}
-    };
-
-    pcb.addConnection(0, 1, 250.0);
-    pcb.addConnection(0, 3, 310.0);
-    pcb.addConnection(1, 2, 260.0);
-    pcb.addConnection(1, 3, 380.0);
-    pcb.addConnection(1, 4, 330.0);
-    pcb.addConnection(2, 4, 220.0);
-    pcb.addConnection(3, 4, 300.0);
-
-    MSTOptimizer::solveKruskal(pcb); 
-    
-    
-    std::vector<Edge> optimizedPaths = {
-        {0, 1, 250.0}, {1, 2, 260.0}, {2, 4, 220.0}, {3, 4, 300.0}
-    };
+    PCBGraph pcb;
+    std::vector<Edge> mstEdges;
+    double totalCost = 0.0;
+    bool isOptimized = false;
 
     while (!WindowShouldClose()) {
+        
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            Vector2 mousePos = GetMousePosition();
+            if (mousePos.y > 70) {
+                pcb.addNode(mousePos.x, mousePos.y);
+                isOptimized = false; 
+                mstEdges.clear();
+            }
+        }
+
+        if (IsKeyPressed(KEY_SPACE) && pcb.nodes.size() > 1) {
+            mstEdges = MSTOptimizer::solveKruskal(pcb, totalCost);
+            isOptimized = true;
+        }
+
+        if (IsKeyPressed(KEY_R)) {
+            pcb.clear();
+            mstEdges.clear();
+            totalCost = 0.0;
+            isOptimized = false;
+        }
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText("PCB Minimum Spanning Tree (Kruskal)", 20, 20, 20, DARKGRAY);
+        DrawRectangle(0, 0, screenWidth, 60, DARKGRAY);
+        DrawText("Fare Sol Tik: Bilesen Ekle  |  SPACE: Optimizasyonu Calistir  |  R: Sifirla", 20, 20, 20, LIGHTGRAY);
 
         for (const auto& edge : pcb.edges) {
-            Vector2 start = { (float)nodes[edge.src].x, (float)nodes[edge.src].y };
-            Vector2 end = { (float)nodes[edge.dest].x, (float)nodes[edge.dest].y };
-            DrawLineEx(start, end, 2.0f, LIGHTGRAY);
+            Vector2 start = { pcb.nodes[edge.src].x, pcb.nodes[edge.src].y };
+            Vector2 end = { pcb.nodes[edge.dest].x, pcb.nodes[edge.dest].y };
+            DrawLineEx(start, end, 1.0f, LIGHTGRAY);
         }
 
-        for (const auto& edge : optimizedPaths) {
-            Vector2 start = { (float)nodes[edge.src].x, (float)nodes[edge.src].y };
-            Vector2 end = { (float)nodes[edge.dest].x, (float)nodes[edge.dest].y };
-            DrawLineEx(start, end, 5.0f, DARKGREEN);
+        if (isOptimized) {
+            for (const auto& edge : mstEdges) {
+                Vector2 start = { pcb.nodes[edge.src].x, pcb.nodes[edge.src].y };
+                Vector2 end = { pcb.nodes[edge.dest].x, pcb.nodes[edge.dest].y };
+                DrawLineEx(start, end, 4.0f, DARKGREEN);
+            }
+            
+            std::string costText = "Toplam Bakir Maliyeti: " + std::to_string((int)totalCost) + " birim";
+            DrawText(costText.c_str(), 20, screenHeight - 40, 24, MAROON);
         }
 
-        for (const auto& node : nodes) {
-            DrawCircle(node.x, node.y, 20.0f, DARKBLUE);
+        for (const auto& node : pcb.nodes) {
+            DrawCircle(node.x, node.y, 16.0f, DARKBLUE);
             DrawText(std::to_string(node.id).c_str(), node.x - 5, node.y - 10, 20, WHITE);
         }
 
