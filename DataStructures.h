379ce002b18f33aa575c2b4354b7a744cb968dcf
ahmetgolcuq <ahmetgolcuq@gmail.com>
2026-06-03 17:54:02 +0300

    hatayı düzelttim

--- DataStructures.h
+++ DataStructures.h
@@ -1,7 +1,45 @@
 #ifndef DATA_STRUCTURES_H
 #define DATA_STRUCTURES_H
 
-// Kişi 3: Queue, Stack ve Union-Find / Min-Heap sınıfları buraya yazılacak.
-// std kütüphanelerinden sadece vector veya iostream gibi yardımcılar kullanılabilir.
+#include <vector>
+
+class UnionFind {
+private:
+    std::vector<int> parent;
+    std::vector<int> rank;
+
+public:
+    UnionFind(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i; 
+        }
+    }
+
+    int find(int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent[i]); // Path compression
+    }
+
+    bool unite(int i, int j) {
+        int rootI = find(i);
+        int rootJ = find(j);
+        
+        if (rootI != rootJ) {
+            if (rank[rootI] < rank[rootJ]) {
+                parent[rootI] = rootJ;
+            } else if (rank[rootI] > rank[rootJ]) {
+                parent[rootJ] = rootI;
+            } else {
+                parent[rootJ] = rootI;
+                rank[rootI]++;
+            }
+            return true; 
+        }
+        return false; 
+    }
+};
 
 #endif
